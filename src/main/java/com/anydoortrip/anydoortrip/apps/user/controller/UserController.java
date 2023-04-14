1d8be0de2e0dc4c8fd65fed8a43d56a2df5571b8
liu <1725635281@qq.com>
2023-04-14 11:14:13 +0800

    返回数据

--- src/main/java/com/anydoortrip/anydoortrip/apps/user/controller/UserController.java
+++ src/main/java/com/anydoortrip/anydoortrip/apps/user/controller/UserController.java
@@ -6,12 +6,17 @@ import com.anydoortrip.anydoortrip.apps.user.requestData.MobilePwdLoginReqData;
 import com.anydoortrip.anydoortrip.apps.user.service.UserService;
 import com.anydoortrip.anydoortrip.apps.utlis.BCrypt;
 import com.anydoortrip.anydoortrip.apps.utlis.Jwt;
+import com.anydoortrip.anydoortrip.apps.utlis.Resp;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
@@ -29,13 +34,16 @@ public class UserController {
      * @param mobilePwdLoginReqData 登录数据
      */
     @PostMapping("/mobile_pwd_login")
-    public void mobile_pwd_login(@RequestBody @Valid MobilePwdLoginReqData mobilePwdLoginReqData) {
+    public Resp mobile_pwd_login(@RequestBody @Valid MobilePwdLoginReqData mobilePwdLoginReqData) {
         Integer a = 1;
         User user = userService.getById(a);
         String token = Jwt.createToken(user);
+        LinkedHashSet<Object> objects = new LinkedHashSet<>();
+        objects.add(user);
+        objects.add(token);
         System.out.println(token);
         BCrypt bCrypt = new BCrypt();
         System.out.println(bCrypt.get_password_hash("123456789"));
-
+        return Resp.success(objects);
     }
 }
